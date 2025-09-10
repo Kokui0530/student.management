252c11520ab71fb823ec96178ca22abdbec5cbdd
小久井　杏奈 <a591.k.0530@gmail.com>
2025-09-10 19:52:44 +0900

    修正

--- src/test/java/raisetech/student/management/controller/converter/StudentConverterTest.java
+++ src/test/java/raisetech/student/management/controller/converter/StudentConverterTest.java
@@ -167,7 +167,7 @@ class StudentConverterTest {
     student.setAge(41);
     student.setGender("男");
     student.setRemark("");
-    student.setDeleted(false);
+    student.setIsDeleted("在籍");
     return student;
   }
 }
